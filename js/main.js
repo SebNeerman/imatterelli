106318f67d886b78488537a9dc95868d2132a2ef
Seb Neerman <seb@digital-pencil.com>
2025-05-23 13:38:27 +0200

    Styles and images

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ const languageBtns = document.querySelectorAll('.lang-btn');
 const mobileLangBtns = document.querySelectorAll('.mobile-lang-btn');
 const sections = document.querySelectorAll('.section');
 const copyrightYear = document.querySelector('.copyright-year');
-<<<<<<< HEAD
 
-=======
->>>>>>> cb444cc4e032ad79241c4ba431c232224b72ca07
 
 // Current language (default: Italian)
 let currentLang = 'it';
